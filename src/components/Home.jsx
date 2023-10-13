5638450891678446567d21bf7af73a7a30f619ac
manojnaraharisetti <manojnaraharisetti2004@gmail.com>
2023-10-13 22:21:33 +0530

    Home page by raja

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import BankImg from "./../assets/images/home.png";
 import TextAni from "./TextAni";
+import carloan from "./../assets/gifs/carloan.gif"
+import health from "./../assets/gifs/health.gif"
+import homeloan from "./../assets/gifs/homeloan.gif"
+import loan from "./../assets/gifs/loan.gif"
+import payment from "./../assets/gifs/payment.gif"
 
 export default function Home() {
   return (
@@ -24,32 +29,60 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div
-          className="col-lg-6 col-sm-12 homeright"
-          style={{ backgroundColor: "lavenderblush" }}
-        >
-          <div className="glass-card p-4 m-2">
-            <h3>#1</h3>
-            <p>Instant Personal Loans at your demand!</p>
-          </div>
-          <div className="glass-card p-4 m-2">
-            <h3>#2</h3>
-            <p>Home loans according to RBI guidlines.</p>
+        <div className="col-lg-6 col-sm-12 homeright" style={{ backgroundColor: "lavenderblush" }}>
+          <div className="glass-card  m-2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <h3 className="display-4">&nbsp;#1</h3>
+              </div>
+              <p>“Vajra Bank: Instant Personal Loans, Tailored for Your Aspirations.”</p>
+              <img src={loan} className="image-fluid homedicards" alt="Image1" />
+            </div>
           </div>
 
-          <div className="glass-card p-4 m-2">
-            <h3>#3</h3>
-            <p>Health insurances at lower prices.</p>
+          <div className="glass-card  m-2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <h3 className="display-4">&nbsp;#2</h3>
+              </div>
+              <p>“Vajra Bank: Home Loans, Aligned with RBI Guidelines.”</p>
+              <img src={homeloan} className="image-fluid homedicards" alt="Image2" />
+            </div>
           </div>
-          <div className="glass-card p-4 m-2">
-            <h3>#4</h3>
-            <p>Car loans at cheaper interests!.</p>
+
+          <div className="glass-card  m-2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <h3 className="display-4">&nbsp;#3</h3>
+              </div>
+              <p>“Affordable Health Insurances, Prioritizing Your Well-being.”</p>
+              <img src={health} className="image-fluid homedicards" alt="Image3" />
+            </div>
           </div>
-          <div className="glass-card p-4 m-2">
-            <h3>#5</h3>
-            <p>Faster payments, low processing!</p>
+
+          <div className="glass-card  m-2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <h3 className="display-4">&nbsp;#4</h3>
+              </div>
+              <p>“Vajra Bank: Car Loans, Lower Interest Rates for Your Journey.”</p>
+              <img src={carloan} className="image-fluid homedicards" alt="Image4" />
+            </div>
+          </div>
+
+          <div className="glass-card  m-2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <h3 className="display-4">&nbsp;#5</h3>
+              </div>
+              <p>"Swift Payments, Minimal Processing: Your Time is Valuable."</p>
+              <img src={payment} className="image-fluid homedicards" alt="Image5" />
+            </div>
           </div>
         </div>
+
+
+
       </div>
     </>
   );
